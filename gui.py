be4fa9c4f3c673a3fff324abef2219be2ebd55e7
Piotrek Chmielowski <piotrek.chmielowski@callpage.io>
2018-02-09 21:52:48 +0100

    Control token from GUI

--- gui.py
+++ gui.py
@@ -4,9 +4,18 @@
 import sys
 from PyQt5.QtWidgets import QMainWindow, QPushButton, QApplication
 import redis
+import string
+import random
 
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
+def newToken():
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
+
+def printState():
+    print (r.get('token').decode(), end='')
+    print (' Expired: ' + r.get('expired').decode())
+    print ()
 
 class Example(QMainWindow):
     
@@ -18,27 +27,27 @@ class Example(QMainWindow):
         
     def initUI(self):      
 
-        btn1 = QPushButton("Button 1", self)
+        btn1 = QPushButton("New token", self)
         btn1.move(30, 50)
 
-        btn2 = QPushButton("Button 2", self)
+        btn2 = QPushButton("Expire", self)
         btn2.move(150, 50)
       
-        btn1.clicked.connect(self.buttonClicked)            
-        btn2.clicked.connect(self.buttonClicked)
-        
-        self.statusBar()
-        
+        btn1.clicked.connect(self.newTokenButtonClicked)            
+        btn2.clicked.connect(self.expireTokenButtonClicked)
+
         self.setGeometry(300, 300, 290, 150)
-        self.setWindowTitle('Event sender')
+        self.setWindowTitle('Token')
         self.show()
-        
-        
-    def buttonClicked(self):
-      
-        sender = self.sender()
-        r.set('foo', sender.text() + ' was pressed')
-        self.statusBar().showMessage(sender.text() + ' was pressed')
+    
+    def newTokenButtonClicked(self):
+        r.set('token', newToken())
+        r.set('expired', False)
+        printState()
+
+    def expireTokenButtonClicked(self):
+        r.set('expired', True)
+        printState()
         
         
 if __name__ == '__main__':
