1a4b5254a9efa0e024b7d4bcd75a7dc7ad6347af
Piotrek Chmielowski <piotrek.chmielowski@callpage.io>
2018-02-10 19:57:42 +0100

    Basic Junit test

--- app/src/test/java/net/chmielowski/token/ExampleUnitTest.java
+++ app/src/test/java/net/chmielowski/token/ExampleUnitTest.java
@@ -1,41 +1,123 @@
 package net.chmielowski.token;
 
+import android.support.annotation.NonNull;
+import android.util.Log;
+
 import com.google.gson.GsonBuilder;
 
 import org.junit.Test;
 
-import okhttp3.HttpUrl;
+import java.io.IOException;
+import java.util.Objects;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+import static java.util.Objects.requireNonNull;
+
 public class ExampleUnitTest {
+
+
+    private static final String AUTHORIZATION = "Authorization";
+    private Api api;
+    private boolean tokenExpired;
+
     @Test
-    public void test() throws Exception {
+    public void getsData() throws Exception {
         MockWebServer server = new MockWebServer();
-
-        final Api.Data data = new Api.Data();
-        data.name = "Hello";
         server.enqueue(new MockResponse().setBody("{ \"name\": \"Hello\" }"));
-
         server.start();
 
-        HttpUrl baseUrl = server.url("/get/");
-
-        new Retrofit.Builder()
-                .baseUrl(baseUrl)
+        final Api api = new Retrofit.Builder()
+                .baseUrl(server.url("/get/"))
+                .client(new OkHttpClient.Builder()
+                        .addInterceptor(this::addToken)
+                        .addInterceptor(this::refreshToken)
+                        .build())
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create(new GsonBuilder().create()))
-                .build()
 
-                .create(TestService.class)
-                .get()
+                .build()
+                .create(Api.class);
+
+        api.data()
                 .test()
-                .assertValue(response -> response.body().equals(data));
+                .assertValue(response -> {
+                    final Api.Data data = new Api.Data();
+                    data.name = "Hello";
+                    return data.equals(response.body());
+                });
 
         server.shutdown();
     }
 
+    private Response refreshToken(final Interceptor.Chain chain) throws IOException {
+        final Request request = chain.request();
+        final Request.Builder builder = request.newBuilder();
+        final Response response = chain.proceed(builder.build());
+        if (response.code() == 401) {
+            final boolean sentWithOldToken = !Objects.equals(requireNonNull(request.header(AUTHORIZATION)), token());
+            if (sentWithOldToken) {
+                return retryWithNewToken(chain, builder);
+            }
+
+            tokenExpired = true;
+            final String before = token();
+            doRefreshToken();
+            Log.d("pchm", before + " => " + token());
+            builder.header(AUTHORIZATION, token());
+            final Request updatedRequest = builder.build();
+            return chain.proceed(updatedRequest);
+        }
+        if (response.code() == 403) {
+            final String token = token();
+            final boolean sentWithOldToken = !Objects.equals(requireNonNull(request.header(AUTHORIZATION)), token);
+            if (sentWithOldToken) {
+                return retryWithNewToken(chain, builder);
+            } else {
+                throw new RuntimeException("403 for current token");
+            }
+        }
+        return response;
+    }
+
+    private Response retryWithNewToken(Interceptor.Chain chain, Request.Builder builder) throws IOException {
+        builder.header(AUTHORIZATION, token());
+        return chain.proceed(builder.build());
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    private synchronized void doRefreshToken() {
+        if (!tokenExpired) {
+            return;
+        }
+        this.storeToken(api.refresh()
+                .blockingGet()
+                .body()
+                .token);
+        tokenExpired = false;
+    }
+
+    private Response addToken(Interceptor.Chain chain) throws IOException {
+        return chain.proceed(chain.request()
+                .newBuilder()
+                .header(AUTHORIZATION, this.token())
+                .build());
+    }
+
+    @NonNull
+    private String token() {
+        return "ABCD";
+    }
+
+    private void storeToken(@NonNull String token) {
+        // TODO
+    }
 }
