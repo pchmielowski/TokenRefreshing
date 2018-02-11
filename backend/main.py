07a2b304dca4cdd856c32bf065bbedc41f22455a
Piotrek Chmielowski <piotrek.chmielowski@callpage.io>
2018-02-11 12:21:08 +0100

    Log token comparison

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,28 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, logging
 import redis
 import json
 import string
 import random
+import typing
 
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 app = Flask(__name__)
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
 
 def newToken():
     return ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
 
+def isTokenValid(request) -> bool:
+    actual = request.headers.get('Authorization')
+    expected = r.get('token').decode()
+    print ("Comparing {} with expected {}".format(actual, expected))
+    return actual == expected
+
 @app.route("/data", methods=['GET'])
 def data():
-    if request.headers.get('Authorization') != r.get('token').decode('utf-8'):
-        print ('403 Expected: {}, actual: {}'.format(r.get('token').decode('utf-8'), request.headers.get('Authorization')))
+    if not isTokenValid(request):
         return "Invalid token", 403
     if r.get('expired') == b'True':
         return "Expired token", 401
@@ -22,8 +30,7 @@ def data():
 
 @app.route("/refresh", methods=['POST'])
 def refresh():
-    if request.headers.get('Authorization') != r.get('token').decode('utf-8'):
-        print ('403 Expected: {}, actual: {}'.format(r.get('token').decode('utf-8'), request.headers.get('Authorization')))        
+    if not isTokenValid(request):
         return "Invalid token", 403
     r.set('expired', False)
     token = newToken()
